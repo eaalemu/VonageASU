2a442a489705760e312e7617f50584144915a451
Emmanuel Alemu <ealemu35@gmail.com>
2023-09-21 14:41:09 -0700

    Emmanuel UI overhaul

---
+++ script.js
@@ -0,0 +1,14 @@
+const body = document.querySelector('body'),
+  closeBtn = document.querySelector('.btn-close-right'),
+  rightSide = document.querySelector('.right-side'),
+  expandBtn = document.querySelector('.expand-btn');
+
+
+closeBtn.addEventListener('click', () => {
+  rightSide.classList.remove('show');
+  expandBtn.classList.add('show');
+});
+expandBtn.addEventListener('click', () => {
+  rightSide.classList.add('show');
+  expandBtn.classList.remove('show');
+});
