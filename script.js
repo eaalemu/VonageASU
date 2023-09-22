1dcdc09996e7f085244ae4f55342047ac6dfe246
dev_Mahesh <mbgali@asu.edu>
2023-09-21 20:24:01 -0700

    Mahesh and Deepika

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-const body = document.querySelector('body'),
-  closeBtn = document.querySelector('.btn-close-right'),
-  rightSide = document.querySelector('.right-side'),
-  expandBtn = document.querySelector('.expand-btn');
+// Get the chat input field, send button, and chat messages container
+const chatInput = document.querySelector('.chat-input');
+const sendButton = document.querySelector('.send-button');
+const chatMessages = document.querySelector('.chat-area');
 
+// Function to send a message
+function sendMessage() {
+  const messageText = chatInput.value.trim();
 
-closeBtn.addEventListener('click', () => {
-  rightSide.classList.remove('show');
-  expandBtn.classList.add('show');
-});
-expandBtn.addEventListener('click', () => {
-  rightSide.classList.add('show');
-  expandBtn.classList.remove('show');
+  if (messageText !== '') {
+    // Create a new message element
+    const messageElement = document.createElement('div');
+    messageElement.classList.add('message');
+    messageElement.textContent = messageText;
+
+    // Append the message element to the chat messages container
+    chatMessages.appendChild(messageElement);
+
+    // Clear the input field
+    chatInput.value = '';
+
+    // Scroll to the bottom of the chat messages
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+  }
+}
+
+// Event listeners for sending a message
+sendButton.addEventListener('click', sendMessage);
+chatInput.addEventListener('keyup', (event) => {
+  if (event.key === 'Enter') {
+    sendMessage();
+  }
 });
